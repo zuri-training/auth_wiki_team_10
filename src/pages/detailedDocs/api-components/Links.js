550df5e59a968dbb7bc76c0819b5eeb1353998c8
Falence Lemungoh <falencelemungoh@gmail.com>
2022-08-12 08:51:02 +0100

    fix download url

--- src/pages/detailedDocs/api-components/Links.js
+++ src/pages/detailedDocs/api-components/Links.js
@@ -8,10 +8,12 @@ const Links = ({ downloadLink }) => {
     return (
         <>
             <div className='download__links'>
-                <Link to={""}> <img src={phpIcon} alt="php" /> <span>Get php code</span> </Link>
-                <Link to={""}>  <img src={NodejsIcon} alt="nodejs" /> <span>Get nodejs code</span></Link>
+                {/* <Link to={""}> <img src={phpIcon} alt="php" /> <span>Get php code</span> </Link> */}
+                {/* <Link to={""}>  <img src={NodejsIcon} alt="nodejs" /> <span>Get nodejs code</span></Link> */}
                 {/* <Link to={downloadLink} target="_blank">  <img src={PythonIcon} alt="php" /> <span>Get python code</span></Link> */}
-                <a href={downloadLink} target="_blank" rel="noreferrer"><img src={PythonIcon} alt="php" /> <span>Get python code</span></a>
+                <a href={downloadLink} target="_blank" rel="noreferrer">  <img src={phpIcon} alt="php" /> <span>Get php code</span>  </a>
+                <a href={downloadLink} target="_blank" rel="noreferrer">  <img src={NodejsIcon} alt="nodejs" /> <span>Get nodejs code</span>  </a>
+                <a href={downloadLink} target="_blank" rel="noreferrer">  <img src={PythonIcon} alt="python" /> <span>Get python code</span>  </a>
 
             </div>
 
