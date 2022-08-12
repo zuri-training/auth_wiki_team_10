ce3e56c240cd86cdef5eb7d5fcde6875e777b34c
Falence Lemungoh <falencelemungoh@gmail.com>
2022-08-12 10:25:13 +0100

    FIx links

--- src/pages/detailedDocs/api-components/Links.js
+++ src/pages/detailedDocs/api-components/Links.js
@@ -4,16 +4,16 @@ import NodejsIcon from '../../../assets/images/Nodejs.png'
 import phpIcon from '../../../assets/images/php.png'
 import PythonIcon from '../../../assets/images/python.png'
 
-const Links = ({ downloadLink }) => {
+const Links = ({ downloadLinkPython,downloadLinkNode, downloadLinkPhp }) => {
     return (
         <>
             <div className='download__links'>
                 {/* <Link to={""}> <img src={phpIcon} alt="php" /> <span>Get php code</span> </Link> */}
                 {/* <Link to={""}>  <img src={NodejsIcon} alt="nodejs" /> <span>Get nodejs code</span></Link> */}
                 {/* <Link to={downloadLink} target="_blank">  <img src={PythonIcon} alt="php" /> <span>Get python code</span></Link> */}
-                <a href={downloadLink} target="_blank" rel="noreferrer">  <img src={phpIcon} alt="php" /> <span>Get php code</span>  </a>
-                <a href={downloadLink} target="_blank" rel="noreferrer">  <img src={NodejsIcon} alt="nodejs" /> <span>Get nodejs code</span>  </a>
-                <a href={downloadLink} target="_blank" rel="noreferrer">  <img src={PythonIcon} alt="python" /> <span>Get python code</span>  </a>
+                <a href={downloadLinkPhp} target="_blank" rel="noreferrer">  <img src={phpIcon} alt="php" /> <span>Get php code</span>  </a>
+                <a href={downloadLinkNode} target="_blank" rel="noreferrer">  <img src={NodejsIcon} alt="nodejs" /> <span>Get nodejs code</span>  </a>
+                <a href={downloadLinkPython} target="_blank" rel="noreferrer">  <img src={PythonIcon} alt="python" /> <span>Get python code</span>  </a>
 
             </div>
 
