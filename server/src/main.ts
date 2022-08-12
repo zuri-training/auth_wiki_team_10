185f52801c478c2cba3ee22105a36dcda100deb1
Falence Lemungoh <falencelemungoh@gmail.com>
2022-08-12 04:43:46 +0100

    fix port

--- server/src/main.ts
+++ server/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   // const document = SwaggerModule.createDocument(app, config);
   // SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 8800);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
